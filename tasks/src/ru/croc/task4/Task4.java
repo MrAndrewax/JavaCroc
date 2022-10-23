b07ade1c83d0a9ee59ee1dd7549ee2b6d2a1dc58
Andrew <mannazarov@yandex.ru>
2022-10-23 15:15:59 +0300

    add Task3.java and Task4.java

---
+++ tasks/src/ru/croc/task4/Task4.java
@@ -0,0 +1,91 @@
+/* Условие:
+    В текстах программ на Java могут использоваться многострочные и однострочные (// ...) комментарии.
+    Реализовать метод, принимающий на вход строковую переменную - исходный текст программы на Java,
+    вырезающий из этой строки все комментарии и возвращающий результат в виде строки.
+ */
+
+/*В качестве исходного текста для задачи я взял этот код*/
+
+public class Task4{
+    public static void main(String[] args) {
+        //String sourceText = "hard code\n//small comment \n very very hard code \n /*bigger \n comment \n */ \n lol";
+        String sourceText = """
+                /* Условие:
+                    В текстах программ на Java могут использоваться многострочные и однострочные (// ...) комментарии.
+                    Реализовать метод, принимающий на вход строковую переменную - исходный текст программы на Java,
+                    вырезающий из этой строки все комментарии и возвращающий результат в виде строки.
+                 */
+                                
+                /*В качестве исходного текста для задачи я взял этот код*/
+                                
+                public class Main {
+                    public static void main(String[] args) {
+                        int start = -1;
+                        int end = -1;
+                        String tempStr;
+                                
+                        while (true){
+                            start = sourceText.indexOf("");//находим начало комментария.
+                            if (start == -1){
+                                break;
+                            }
+                            end = sourceText.substring(start).indexOf("\\n");//находим индекс первого вхождения "\\n" после начала комментария.
+                            if (end == -1){//если конец строки не найден, то это последняя строка. Тогда за конец комментария возьмём конец текста.
+                                end = sourceText.length();
+                            }
+                            tempStr = sourceText.substring(start, end + start);// Сейчас tempStr ссылается на однострочный комментарий
+                            sourceText = sourceText.replace(tempStr, "");//удаляем найденный комментарий.
+                        }
+                                
+                        /*Проводим аналогичную операцию
+                            с многострочными комментариями */
+                        while (true){
+                            start = sourceText.indexOf("");
+                            if (start == -1){
+                                break;
+                            }
+                            tempStr = sourceText.substring(start);
+                            end = tempStr.indexOf("");
+                            tempStr = sourceText.substring(start, end + start + 2);//Сейчас tempStr ссылается на многострочный комментарий. Добавляем к концу строку 2, чтобы захватить */
+                            sourceText = sourceText.replace(tempStr, "");
+                        }
+                                
+                        System.out.println(sourceText);
+                    }
+                }
+                """;
+
+
+        int start = -1;
+        int end = -1;
+        String tempStr;
+
+        while (true){
+            start = sourceText.indexOf("//");//находим начало комментария.
+            if (start == -1){
+                break;
+            }
+            end = sourceText.substring(start).indexOf("\n");//находим индекс первого вхождения "\n" после начала комментария.
+            if (end == -1){//если конец строки не найден, то это последняя строка. Тогда за конец комментария возьмём конец текста.
+                end = sourceText.length();
+            }
+            tempStr = sourceText.substring(start, end + start);// Сейчас tempStr ссылается на однострочный комментарий
+            sourceText = sourceText.replace(tempStr, "");//удаляем найденный комментарий.
+        }
+
+        /*Проводим аналогичную операцию
+            с многострочными комментариями */
+        while (true){
+            start = sourceText.indexOf("/*");
+            if (start == -1){
+                break;
+            }
+            tempStr = sourceText.substring(start);
+            end = tempStr.indexOf("*/");
+            tempStr = sourceText.substring(start, end + start + 2);//Сейчас tempStr ссылается на многострочный комментарий. Добавляем к концу строку 2, чтобы захватить */
+            sourceText = sourceText.replace(tempStr, "");
+        }
+
+        System.out.println(sourceText);
+    }
+}
