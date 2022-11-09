ea8b354570e33f839d5543ebe2a4b2beef0153d0
Andrew <mannazarov@yandex.ru>
2022-11-09 13:14:12 +0300

    fix task8 with 'try-with-resources'

--- tasks/src/ru/croc/task8/Task8.java
+++ tasks/src/ru/croc/task8/Task8.java
@@ -6,13 +6,16 @@ import java.io.FileReader;
 public class Task8 {
     public static void main(String[] args) throws IOException {
         String path = args[0];
-
-        FileReader reader = new FileReader(path);
-        StringBuilder textFromFile = new StringBuilder("");
-        int c;
-        while( (c = reader.read()) != -1 ){
-            textFromFile.append((char) c);
+        StringBuilder textFromFile;
+        
+        try (FileReader reader = new FileReader(path)){
+            textFromFile = new StringBuilder("");
+            int c;
+            while ((c = reader.read()) != -1) {
+                textFromFile.append((char) c);
+            }
         }
+
         String clearText = String.valueOf(textFromFile).trim()
                 .replaceAll("\n"," ")//заменяем символы переноса строки на пробелы
                 .replaceAll(" +", " ");//удаляем дубликаты пробелов
