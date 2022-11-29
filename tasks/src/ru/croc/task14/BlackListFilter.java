b9062e63d846cd5a73c0f6ba3915c6783a7903f8
Andrew <mannazarov@yandex.ru>
2022-11-29 14:30:37 +0300

    fix task14

--- tasks/src/ru/croc/task14/BlackListFilter.java
+++ tasks/src/ru/croc/task14/BlackListFilter.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 interface BlackListFilter{
+    //C - тип коллекции, Т - тип элементов коллекции.
     default <T, C extends Iterable<T>> void filterComments(C collection, Set<String> blackList, Predicate<T> filterCondition){
         Iterator<T> commentsIterator = collection.iterator();
         while (commentsIterator.hasNext()){
