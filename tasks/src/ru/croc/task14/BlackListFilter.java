8a11b6e6ae06116d377d88137972ad306b98a570
Andrew <mannazarov@yandex.ru>
2022-12-03 12:46:05 +0300

    fix task14

--- tasks/src/ru/croc/task14/BlackListFilter.java
+++ tasks/src/ru/croc/task14/BlackListFilter.java
@@ -1,18 +1,34 @@
 package ru.croc.task14;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.function.Predicate;
 
 interface BlackListFilter{
-    //C - тип коллекции, Т - тип элементов коллекции.
-    default <T, C extends Iterable<T>> void filterComments(C collection, Set<String> blackList, Predicate<T> filterCondition){
-        Iterator<T> commentsIterator = collection.iterator();
-        while (commentsIterator.hasNext()){
-            boolean isBadWordInComment = filterCondition.test(commentsIterator.next());
-            if (isBadWordInComment){
-                commentsIterator.remove();
+    //Т - тип элементов коллекции.
+    /**
+     * From the given list of comments removes ones
+     * that contain words from the black list.
+     *
+     * @param collectionWithComments - Произовольная коллекция элементов типа T,
+     *                               которая имплементирует интерфейс Iterable
+     * @param filterCondition - Предикат, который возвращает True в случае,
+     *                        если комментарий содержит запрещенное слово,
+     *                        иначе False.
+     * @return Произвольная коллекция типа T
+     */
+    default <T> Collection<T> filterComments(Iterable<T> collectionWithComments, Predicate<T> filterCondition){
+
+        Collection<T> clearComments = new ArrayList<>();
+
+        for (T comment : collectionWithComments) {
+            boolean isBadWordInComment = filterCondition.test(comment);
+            if (!isBadWordInComment) {
+                clearComments.add(comment);
             }
         }
+        return clearComments;
     }
 }
